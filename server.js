dc1a8c2648a7c710ae28db6f9e865a00e7be290a
Vedansh Dubey <vedanshdubeyd6@gmail.com>
2026-01-12 10:24:22 +0530

    Update server.js
    Updated port location to bind to 0.0.0.0 for error handling of as to why the server is crashing

--- server.js
+++ server.js
@@ -143,8 +143,8 @@ app.get('/', (req, res) => {
 });
 
 // Start Express server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
   // Run an immediate check on startup
   checkSensors();
-});
+});;
